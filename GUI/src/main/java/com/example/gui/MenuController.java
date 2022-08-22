38ce09ddb3c8570bee06df67fa3f687289a5aa3b
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-08-22 13:46:31 -0500

    Working on basic display menu.

---
+++ GUI/src/main/java/com/example/gui/MenuController.java
@@ -0,0 +1,26 @@
+package com.example.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.ToolBar;
+
+public class MenuController {
+    @FXML
+    TableColumn col;
+
+    @FXML
+    ToolBar tool;
+    @FXML
+    Button buttonTwo;
+    @FXML
+    Button buttonThree;
+    @FXML
+    Button buttonCustom;
+
+    @FXML
+    void initialize(){
+        
+    }
+}
